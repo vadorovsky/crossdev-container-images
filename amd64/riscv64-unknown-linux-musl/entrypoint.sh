3443a8a5bd0d775cfe07296b5a3120898845b47c
vad <vad.sol@proton.me>
2026-01-08 10:27:44 +0100

    dockerfile, ci: Use QEMU as user-space emulator for running Rust tests

--- amd64/riscv64-unknown-linux-musl/entrypoint.sh
+++ amd64/riscv64-unknown-linux-musl/entrypoint.sh
@@ -2,5 +2,6 @@
 
 source /etc/profile
 source /env
+CARGO_TARGET_RISCV64_UNKNOWN_LINUX_MUSL_RUNNER=qemu-aarch64
 
 exec "$@"
--- arm64/x86_64-unknown-linux-musl/entrypoint.sh
+++ arm64/x86_64-unknown-linux-musl/entrypoint.sh
@@ -2,5 +2,6 @@
 
 source /etc/profile
 source /env
+CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_RUNNER=qemu-x86_64
 
 exec "$@"
